23322be81b58f9847be29c7d89d5fe9132f3fae9
agamswaroop619 <agam.swaroop619@gmail.com>
2023-12-19 19:03:00 +0530

    fixes: minor fixes

--- sarvgangevani/src/app/page.tsx
+++ sarvgangevani/src/app/page.tsx
@@ -1,11 +1,10 @@
 import Navbar from "./components/navbar";
 import Ask from "./components/ask_anything_chachaji";
 import Chachajifull from "./components/chachaji_image";
-import Navbtn from "./components/navbtn";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between min-w-full bg-gradient-to-r from-[#ABEE80] via-[#3DB6F5] to-[#002648] absolute">
+    <main className="flex min-h-screen flex-col items-center justify-between min-w-full bg-gradient-to-b from-[#ABEE80] via-[#6ACCFF] to-[#0061B8] absolute">
       <Navbar />
       <Ask />
       <Chachajifull />
