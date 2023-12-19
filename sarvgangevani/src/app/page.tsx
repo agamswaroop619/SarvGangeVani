13caa562407dc1be24f983021e261a6ad989cdc6
agamswaroop619 <agam.swaroop619@gmail.com>
2023-12-20 05:24:17 +0530

    add: Chachaji front page fixes

--- sarvgangevani/src/app/page.tsx
+++ sarvgangevani/src/app/page.tsx
@@ -1,51 +1,91 @@
 import Image from "next/image";
 import Chachaji from "./components/chacha-removebg-preview.jpg";
 import Boat from "./components/boat.png";
+import Waves1 from "./components/Vector.png";
+import Waves2 from "./components/image 8.jpg";
+import Shore from "./components/Ellipse 2.jpg";
 export default function Home() {
   return (
     <main className="min-h-screen items-center justify-between min-w-full bg-gradient-to-b from-[#FFFFFF] to-[#39B5E9] absolute">
-        <div className="absolute inset-0 m-auto flex flex-col items-center justify-center">
-          <div className="z-10">
-            <h1 className="text-5xl font-bold text-black font-mono absolute top-0 p-4 left-0 w-1/3">SARV GANGE VANI</h1>
-          </div>
-          <div className="z-10 flex space-x-4 absolute top-0 px-4 py-6 right-0">
-            <a href="/games" className="text-black text-lg">
-              Games
-            </a>
-            <a href="/quiz" className="text-black text-lg">
-              Quiz
-            </a>
-            <a href="/news" className="text-black text-lg">
-              News
-            </a>
-            <a href="/about" className="text-black text-lg">
-              About Us
-            </a>
-          </div>
-          <div className="z-10 mt-12">
-            <a href="">
+      <div className="absolute inset-0 m-auto flex flex-col items-center justify-center">
+        <div className="z-10">
+          <h1 className="text-5xl font-bold text-black font-mono absolute top-0 p-4 left-0 w-1/3">
+            SARV GANGE VANI
+          </h1>
+        </div>
+        <div className="z-10 flex space-x-4 absolute top-0 px-4 py-6 right-0">
+          <a href="/games" className="text-black text-lg">
+            Games
+          </a>
+          <a href="/quiz" className="text-black text-lg">
+            Quiz
+          </a>
+          <a href="/news" className="text-black text-lg">
+            News
+          </a>
+          <a href="/about" className="text-black text-lg">
+            About Us
+          </a>
+        </div>
+        <div className="z-10 mt-12">
+          <a href="">
             <button className="bg-white text-blue-700 px-6 py-3 rounded-full text-lg font-semibold shadow-lg">
               Click to start!
             </button>
-            </a>
-          </div>
-          <div className="absolute bottom-0 left-0 w-1/3">
-            <Image
-              src={Chachaji}
-              alt="Illustration of Chachaji in traditional attire dancing"
-              width={400}
-              height={400}
-            />
-          </div>
-          <div className="absolute bottom-0 right-0 w-1/3">
-            <Image
-             src={Boat}
-              alt="Illustration of children in a boat with balloons and a flag"
-              width={500}
-              height={500}
-            />
-          </div>
+          </a>
         </div>
+        <div className="absolute bottom-0 left-0 w-1/3">
+          <Image
+            src={Chachaji}
+            alt="Illustration of Chachaji in traditional attire dancing"
+            width={400}
+            height={400}
+          />
+        </div>{" "}
+        <Image
+          src={Waves1}
+          alt=""
+          width={800}
+          height={700}
+          className="absolute bottom-0 right-0"
+        />
+        <Image
+          src={Waves1}
+          alt=""
+          width={800}
+          height={700}
+          className="absolute bottom-0 right-[20vw] "
+        />
+        <Image
+          src={Waves2}
+          alt=""
+          width={800}
+          height={700}
+          className="absolute bottom-0 right-[10vw] mix-blend-multiply z-10"
+        />
+        <Image
+          src={Waves2}
+          alt=""
+          width={800}
+          height={700}
+          className="absolute bottom-0 right-[-10vw] mix-blend-multiply z-10"
+        />
+        <Image
+          src={Shore}
+          alt=""
+          width={800}
+          height={700}
+          className="absolute bottom-[-50vh] left-[-10vw] mix-blend-multiply z-10 scale-75"
+        />
+        <div className="absolute bottom-0 right-0 w-1/3">
+          <Image
+            src={Boat}
+            alt="Illustration of children in a boat with balloons and a flag"
+            width={500}
+            height={500}
+          />
+        </div>
+      </div>
     </main>
   );
 }
