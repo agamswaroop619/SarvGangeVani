d18b2dedd001f7f34dd2353881706e081b4a480b
agamswaroop619 <agam.swaroop619@gmail.com>
2023-12-20 02:39:02 +0530

    fix: frontend changes

--- sarvgangevani/src/app/page.tsx
+++ sarvgangevani/src/app/page.tsx
@@ -1,21 +1,23 @@
 import Image from "next/image";
 import Chachaji from "./components/chacha-removebg-preview.jpg";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between min-w-full bg-gradient-to-b from-[#FFFFFF] to-[#39B5E9] absolute">
-      <div
+    <main className="min-h-screen items-center justify-between min-w-full bg-gradient-to-b from-[#FFFFFF] to-[#39B5E9] absolute">
+      <section
         style={{
-          textAlign: "right",
+          textAlign: "left", // "align" should be "textAlign"
           color: "#0D1A5C",
           fontSize: 36,
-          fontFamily: "Righteous",
+          fontFamily: "monospace",
           fontWeight: "400",
           wordWrap: "break-word",
         }}
+        className="px-2"
       >
-        SARV GANGE VANI
-      </div>
-      <Image src={Chachaji} alt="" />
+        <div>SARV GANGE VANI</div>
+      </section>
+      <Image src={Chachaji} alt="" className="bottom-0" />
     </main>
   );
 }
