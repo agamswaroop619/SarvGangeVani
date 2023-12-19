edbcf8b4914bac82fe9e5a33229076f8775dafe6
Aarat Swaroop <aarat2018@gmail.com>
2023-12-20 03:44:15 +0530

    fix: frontend updates

--- sarvgangevani/src/app/page.tsx
+++ sarvgangevani/src/app/page.tsx
@@ -6,24 +6,24 @@ export default function Home() {
     <main className="min-h-screen items-center justify-between min-w-full bg-gradient-to-b from-[#FFFFFF] to-[#39B5E9] absolute">
         <div className="absolute inset-0 m-auto flex flex-col items-center justify-center">
           <div className="z-10">
-            <h1 className="text-6xl font-bold text-white">SARV GANGE VANI</h1>
+            <h1 className="text-5xl font-bold text-black font-mono absolute top-0 p-4 left-0 w-1/3">SARV GANGE VANI</h1>
           </div>
-          <div className="z-10 flex space-x-4 mt-8">
-            <a href="/games" className="text-white text-lg">
+          <div className="z-10 flex space-x-4 absolute top-0 px-4 py-6 right-0">
+            <a href="/games" className="text-black text-lg">
               Games
             </a>
-            <a href="/quiz" className="text-white text-lg">
+            <a href="/quiz" className="text-black text-lg">
               Quiz
             </a>
-            <a href="/news" className="text-white text-lg">
+            <a href="/news" className="text-black text-lg">
               News
             </a>
-            <a href="/about" className="text-white text-lg">
+            <a href="/about" className="text-black text-lg">
               About Us
             </a>
           </div>
           <div className="z-10 mt-12">
-            <a href="/chat">
+            <a href="">
             <button className="bg-white text-blue-700 px-6 py-3 rounded-full text-lg font-semibold shadow-lg">
               Click to start!
             </button>
@@ -33,8 +33,8 @@ export default function Home() {
             <Image
               src={Chachaji}
               alt="Illustration of Chachaji in traditional attire dancing"
-              width={500}
-              height={500}
+              width={400}
+              height={400}
             />
           </div>
           <div className="absolute bottom-0 right-0 w-1/3">
