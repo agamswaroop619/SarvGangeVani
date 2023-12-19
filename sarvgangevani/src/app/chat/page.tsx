328fbd55e2c8cf1b5a4a5850cde6a82ade44d208
agamswaroop619 <agam.swaroop619@gmail.com>
2023-12-19 16:53:14 +0530

    add: prod code stage

--- sarvgangevani/src/app/chat/page.tsx
+++ sarvgangevani/src/app/chat/page.tsx
@@ -1,11 +1,12 @@
-// Home.tsx
 "use client";
 import React, { useEffect, useState } from "react";
 import Navbar from "../components/navbar";
 import Textinput from "../components/textinput";
 import Messages from "../components/messages";
+import Image from "next/image";
+import Namami from "../components/namami_gange.gif";
 
-function getChat(prompt: string = "Give a small introduction in of yours") {
+function getChat(prompt: string) {
   return fetch("/api/chat", {
     method: "POST",
     headers: {
@@ -18,36 +19,58 @@ function getChat(prompt: string = "Give a small introduction in of yours") {
 }
 
 export default function Home() {
-  const [receivedMessages, setReceivedMessages] = useState<string[]>([]);
+  const [receivedMessages, setReceivedMessages] = useState<
+    { text: string; fromUser: boolean }[]
+  >([]);
   const [currentQuestion, setCurrentQuestion] = useState<string>(
-    "Give a small introduction in of yours"
+    "Give a small introduction of yours in 10 words"
   );
 
-  const handleReceiveMessage = (message: string) => {
-    // Update the array of received messages
-    setReceivedMessages((prevMessages) => [...prevMessages, message]);
+  const handleReceiveMessage = (message: string, fromUser: boolean) => {
+    setReceivedMessages((prevMessages) => [
+      ...prevMessages,
+      { text: message, fromUser },
+    ]);
   };
 
-  const updateMessages = () => {
-    // Fetch the response for the updated question and update messages
-    getChat(currentQuestion).then((response) => {
-      handleReceiveMessage(response);
+  const updateMessages = (question: string) => {
+    handleReceiveMessage(question, true); // User's question
+    getChat(question).then((response) => {
+      handleReceiveMessage(response, false); // Chatbot's response
     });
   };
 
+  let temp_fix = 0;
   useEffect(() => {
-    updateMessages(); // Fetch initial message when the component mounts
-  }, []); // Empty dependency array to run only once when the component mounts
+    if (temp_fix === 0) temp_fix++;
+    else {
+      if (typeof window !== "undefined") {
+        getChat(currentQuestion).then((response) => {
+          handleReceiveMessage(response, false);
+        });
+      }
+    }
+  }, [currentQuestion]);
+
+  useEffect(() => {
+    console.log("currentQuestion changed:", currentQuestion);
+    if (currentQuestion !== "Give a small introduction of yours in 10 words") {
+      updateMessages(currentQuestion);
+    }
+  }, [currentQuestion]);
 
   return (
-    <main className="flex min-h-[100vh] flex-col items-center justify-between min-w-[100vw] bg-[#B6D3FE]">
+    <main className="flex min-h-[100vh] flex-col items-center justify-between min-w-[100vw] bg-[#B6D3FE] absolute">
       <Navbar />
+      <div className="flex flex-col items-center justify-center h-screen">
+        <Image
+          src={Namami}
+          alt=""
+          className="mix-blend-multiply opacity-75 z-[0]"
+        />
+      </div>
       <Messages messages={receivedMessages} />
-      <Textinput
-        onReceiveMessage={handleReceiveMessage}
-        setQuestion={setCurrentQuestion} // Pass a function to set the current question
-        updateMessages={updateMessages} // Pass a function to update messages
-      />
+      <Textinput setQuestion={setCurrentQuestion} onAskQuestion={() => {}} />
     </main>
   );
 }
---
+++ sarvgangevani/src/app/news/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Ganges Rejuvenation Milestone: Namami Gange Initiative Achieves Significant Progress in River Cleanup",
+  description: "This news title highlights a positive development in the ongoing efforts of the Namami Gange initiative. It suggests that there has been a noteworthy achievement or milestone in the process of rejuvenating the Ganges River through the program. The term 'significant progress in river cleanup' implies that the initiative has successfully addressed some of the pollution or environmental challenges faced by the Ganges.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
