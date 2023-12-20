d576fbc2cb7afc543f6f32bee2f9d5af56d0f4a7
agamswaroop619 <agam.swaroop619@gmail.com>
2023-12-20 13:09:59 +0530

    add: box referneces

--- sarvgangevani/src/app/games/page.tsx
+++ sarvgangevani/src/app/games/page.tsx
@@ -4,28 +4,24 @@ import { useState } from "react";
 export default function Home() {
   return (
     <main className="min-h-screen flex items-center justify-between min-w-full bg-gradient-to-b from-[#FFFFFF] to-[#39B5E9] absolute p-4">
-      {/* Container for the grid */}
       <div className="flex flex-grow gap-4">
-        {/* Box 1 */}
-        <a href="/g1">
-          <div className="flex-grow bg-white p-4 rounded-md shadow-md h-full">
-            {/* Content of Box 1 */}
-            Box 1
-          </div>
+        <a
+          href="/g1"
+          className="flex-grow bg-white p-4 rounded-md shadow-md h-full"
+        >
+          <div>Box 1</div>
         </a>
-        <a href="/g2">
-          <div className="flex-grow bg-white p-4 rounded-md shadow-md h-full">
-            {/* Content of Box 2 */}
-            Box 2
-          </div>
+        <a
+          href="/g2"
+          className="flex-grow bg-white p-4 rounded-md shadow-md h-full"
+        >
+          <div>Box 2</div>
         </a>
-
-        {/* Box 3 */}
-        <a href="/g3">
-          <div className="flex-grow bg-white p-4 rounded-md shadow-md h-full">
-            {/* Content of Box 3 */}
-            Box 3
-          </div>
+        <a
+          href="/g3"
+          className="flex-grow bg-white p-4 rounded-md shadow-md h-full"
+        >
+          <div>Box 3</div>
         </a>
       </div>
     </main>
