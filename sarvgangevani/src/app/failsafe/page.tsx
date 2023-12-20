4ebae54089e87841b14fc97795089b1ce14896d5
agamswaroop619 <agam.swaroop619@gmail.com>
2023-12-20 08:38:08 +0530

    fix: failsafe started

---
+++ sarvgangevani/src/app/failsafe/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen items-center justify-between min-w-full bg-gradient-to-b from-[#FFFFFF] to-[#39B5E9] absolute">
+      <input
+        type="text"
+        placeholder="Type here"
+        className="input w-full max-w-xs"
+      />
+    </main>
+  );
+}
