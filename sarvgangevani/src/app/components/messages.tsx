05e7257df725b78066b2c5fdf6782a7c25d00231
agamswaroop619 <agam.swaroop619@gmail.com>
2023-12-20 12:43:41 +0530

    add: complete voice chat

--- sarvgangevani/src/app/components/messages.tsx
+++ sarvgangevani/src/app/components/messages.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 interface MessagesProps {
   messages: { text: string; fromUser: boolean }[];
@@ -19,79 +19,60 @@ const Messages: React.FC<MessagesProps> = ({ messages }) => {
     setCurrentQuestion(text);
   };
 
-  if (messages.length === 0) {
-    return (
-      <div className="absolute px-4 scroll-overflow inset-x-0 bottom-[10vh]">
-        <h1 className="text-3xl">Frequently Asked Questions</h1>
-        <div className="grid grid-cols-2 gap-2">
-          <button
-            className="border-double border-4 rounded-full border-white"
-            id="sample1"
-            onClick={() =>
-              handleButtonClick(
-                "What is Namami Gange? How can we contribute to it?"
-              )
-            }
-          >
-            What is Namami Gange? How can we contribute to it?
-          </button>
-          <button
-            className="border-double border-4 rounded-full border-white"
-            id="sample2"
-            onClick={() =>
-              handleButtonClick("Chachaji gaana maa ki koi kahani sunaiye naa!")
-            }
-          >
-            Chachaji gaana maa ki koi kahani sunaiye naa!
-          </button>
-        </div>
-      </div>
-    );
-  } else {
-    return (
-      <div className="relative scroll-overflow top-[5vh] bottom-[10vh]">
-        {messages.map((message, index) => (
-          <div
-            className={`${
-              message.fromUser ? "chat chat-end" : "chat chat-start"
-            } animate-fadeInScale`}
-            key={message.text}
-          >
-            <div className="chat-image avatar">
-              <div className="w-16 h-16 rounded-full">
-                <img
-                  alt={message.fromUser ? "User Avatar" : "Chatbot Avatar"}
-                  src={
-                    message.fromUser
-                      ? "https://cdn-icons-png.flaticon.com/512/1154/1154439.png"
-                      : "https://media.licdn.com/dms/image/C4D0BAQEkWJOoS8iGLA/company-logo_200_200/0/1630516424447?e=2147483647&v=beta&t=KrmED5uJgzXgITjPsFnID5PE7oPNMy5k1BMUwqIlasc"
-                  }
-                  className={message.fromUser ? "scale-100" : "scale-150"}
-                />
-              </div>
-            </div>
-            <div className="chat-footer">
-              {message.fromUser ? "You" : "Chachaji"}
-              <button
-                id="voice"
-                onClick={() => {
-                  speak(message.text);
-                  if (message.fromUser) {
-                    handleButtonClick(message.text);
-                  }
-                }}
-              >
-                🔊
-              </button>
-            </div>
-            <div className="chat-bubble text-xl bg-white text-black">
-              {message.text}
+  useEffect(() => {
+    // Get the voice button element
+    const voiceButton = document.getElementById("voice");
+
+    // Check if there is a new message and if the voice button is available
+    if (messages.length > 0 && voiceButton) {
+      // Programmatically click the voice button
+      voiceButton.click();
+    }
+  }, [messages]);
+
+  return (
+    <div className="relative scroll-overflow top-[5vh] bottom-[10vh]">
+      {messages.map((message, index) => (
+        <div
+          className={`${
+            message.fromUser ? "chat chat-end" : "chat chat-start"
+          } animate-fadeInScale`}
+          key={message.text}
+        >
+          <div className="chat-image avatar">
+            <div className="w-16 h-16 rounded-full">
+              <img
+                alt={message.fromUser ? "User Avatar" : "Chatbot Avatar"}
+                src={
+                  message.fromUser
+                    ? "https://cdn-icons-png.flaticon.com/512/1154/1154439.png"
+                    : "https://media.licdn.com/dms/image/C4D0BAQEkWJOoS8iGLA/company-logo_200_200/0/1630516424447?e=2147483647&v=beta&t=KrmED5uJgzXgITjPsFnID5PE7oPNMy5k1BMUwqIlasc"
+                }
+                className={message.fromUser ? "scale-100" : "scale-150"}
+              />
             </div>
           </div>
-        ))}
-      </div>
-    );
-  }
+          <div className="chat-footer">
+            {message.fromUser ? "You" : "Chachaji"}
+            <button
+              id="voice"
+              onClick={() => {
+                speak(message.text);
+                if (message.fromUser) {
+                  handleButtonClick(message.text);
+                }
+              }}
+            >
+              🔊
+            </button>
+          </div>
+          <div className="chat-bubble text-xl bg-white text-black">
+            {message.text}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Messages;
