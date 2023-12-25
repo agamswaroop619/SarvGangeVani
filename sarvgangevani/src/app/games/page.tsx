5ed0ccdfa7b37add1fbe2c21a1e7aba6daa34052
agamswaroop619 <agam.swaroop619@gmail.com>
2023-12-25 15:05:46 +0530

    add: minor fixes

--- sarvgangevani/src/app/games/page.tsx
+++ sarvgangevani/src/app/games/page.tsx
@@ -1,29 +1,280 @@
-"use client";
-import { useState } from "react";
+// Topic: River Ganga
+"use client"; // Add this line to specify that this component is a Client Component
+import React, { useState, useEffect } from "react";
+import Image from "next/image";
 
-export default function Home() {
-  return (
-    <main className="min-h-screen flex items-center justify-between min-w-full bg-gradient-to-b from-[#FFFFFF] to-[#39B5E9] absolute p-4">
-      <div className="flex flex-grow gap-4">
-        <a
-          href="/g1"
-          className="flex-grow bg-white p-4 rounded-md shadow-md h-full"
-        >
-          <div>Box 1</div>
-        </a>
-        <a
-          href="/g2"
-          className="flex-grow bg-white p-4 rounded-md shadow-md h-full"
-        >
-          <div>Box 2</div>
-        </a>
-        <a
-          href="/g3"
-          className="flex-grow bg-white p-4 rounded-md shadow-md h-full"
-        >
-          <div>Box 3</div>
-        </a>
-      </div>
-    </main>
-  );
+// Types
+interface IKard {
+  id: number;
+  flipped: boolean;
+  matched: boolean;
+  defaultImage: string;
+  image?: string;
 }
+
+// Kard Component
+const Kard: React.FC<{
+  card: IKard;
+  handleFlip: (id: number, matched: boolean) => void;
+}> = ({ card, handleFlip }) => {
+  const handleClick = () => {
+    handleFlip(card.id, card.matched);
+  };
+
+  return (
+    <div key={card.id} className="w-[75px] h-[100px]" onClick={handleClick}>
+      <Image
+        src={card.flipped ? card.image ?? card.defaultImage : card.defaultImage}
+        alt="Card Image"
+        width={75}
+        height={100}
+        className="object-fill h-full rounded"
+      />
+    </div>
+  );
+};
+
+// ListKards Component
+const ListKards: React.FC<{
+  cards: IKard[];
+  attempts: number;
+  setAttempts: (attempts: number) => void;
+  win: boolean;
+  setWin: (win: boolean) => void;
+}> = ({ cards: initialCards, attempts, setAttempts, win, setWin }) => {
+  const [cards, setCards] = useState<IKard[]>(initialCards);
+  const [cardsClicked, setCardsClicked] = useState<number[]>([]);
+  const [canClick, setCanClick] = useState(true);
+
+  const flipCard = (id: number) => {
+    if (
+      !canClick ||
+      cardsClicked.length === 2 ||
+      cardsClicked.includes(id) ||
+      win
+    ) {
+      return;
+    }
+
+    const newCardsClicked = [...cardsClicked, id];
+    setCardsClicked(newCardsClicked);
+
+    const newCards = cards.map((card) => {
+      if (card.id === id) {
+        return {
+          ...card,
+          flipped: !card.flipped,
+        };
+      }
+      return card;
+    });
+
+    setCards(newCards);
+
+    if (newCardsClicked.length === 2) {
+      setCanClick(false);
+
+      const [firstCard, secondCard] = newCardsClicked;
+
+      const firstCardObject = newCards.find(
+        (card) => card.id === firstCard
+      ) as IKard;
+      const secondCardObject = newCards.find(
+        (card) => card.id === secondCard
+      ) as IKard;
+
+      if (firstCardObject.image !== secondCardObject.image) {
+        setTimeout(() => {
+          const resetCards = newCards.map((card) => {
+            if (card.id === firstCard || card.id === secondCard) {
+              return {
+                ...card,
+                flipped: false,
+              };
+            }
+            return card;
+          });
+
+          setCards(resetCards);
+          setCardsClicked([]);
+          setCanClick(true);
+          setAttempts(attempts + 1);
+        }, 1000);
+      } else {
+        const matchedCards = newCards.map((card) => {
+          if (card.id === firstCard || card.id === secondCard) {
+            return {
+              ...card,
+              matched: true,
+            };
+          }
+          return card;
+        });
+
+        setCards(matchedCards);
+
+        const win = matchedCards.every((card) => card.matched);
+
+        if (win) {
+          setWin(true);
+        }
+
+        setAttempts(attempts + 1);
+        setCardsClicked([]);
+        setCanClick(true);
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (cardsClicked.length === 2) {
+      setCanClick(false);
+    } else {
+      setCanClick(true);
+    }
+  }, [cardsClicked]);
+
+  return (
+    <div className="flex flex-row flex-wrap justify-center w-full gap-2">
+      {cards.map((card) => (
+        <Kard key={card.id} card={card} handleFlip={flipCard} />
+      ))}
+    </div>
+  );
+};
+
+// KardForm Component
+const KardForm: React.FC<{
+  theme: string;
+  setTheme: (theme: string) => void;
+  handleGenerateKards: (e: React.FormEvent) => void;
+}> = ({ theme, setTheme, handleGenerateKards }) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTheme(e.target.value);
+  };
+
+  return (
+    <form
+      onSubmit={handleGenerateKards}
+      className="w-full text-center md:w-3/4 lg:w-1/2"
+    >
+      <div className="relative w-full">
+        <input
+          type="text"
+          value={theme}
+          onChange={handleChange}
+          required={true}
+          className="w-full px-4 py-3 text-center placeholder-gray-400 border border-gray-300 rounded-lg focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+          placeholder='Enter theme: e.g., "car ferrari"'
+        />
+        <button
+          type="submit"
+          className="block px-4 py-2 mx-auto mt-4 text-white uppercase bg-blue-500 rounded-lg hover:bg-blue-700"
+        >
+          Generate Cards
+        </button>
+      </div>
+    </form>
+  );
+};
+
+// Game Component
+const Game: React.FC = () => {
+  const {
+    attempts,
+    win,
+    theme,
+    kards,
+    startGame,
+    setTheme,
+    handleGenerateKards,
+    setAttempts,
+    setWin,
+  } = useGameLogic();
+
+  return (
+    <>
+      {!startGame && (
+        <KardForm
+          theme={theme}
+          setTheme={setTheme}
+          handleGenerateKards={handleGenerateKards}
+        />
+      )}
+
+      {startGame && (
+        <>
+          <h2 className="text-xl font-bold text-center text-gray-900">
+            Theme: <strong>{theme}</strong>
+          </h2>
+
+          {!win && (
+            <h2 className="text-xl font-bold text-center text-gray-900">
+              Moves: <strong>{attempts}</strong>
+            </h2>
+          )}
+
+          <ListKards
+            cards={kards}
+            attempts={attempts}
+            setAttempts={setAttempts}
+            win={win}
+            setWin={setWin}
+          />
+
+          {win && (
+            <h2 className="text-xl font-bold text-center text-gray-900">
+              You won in <strong>{attempts}</strong> moves!
+            </h2>
+          )}
+
+          <button className="px-4 py-2 mt-4 text-white uppercase bg-red-500 rounded hover:bg-red-700">
+            <a href="/">Play Again</a>
+          </button>
+        </>
+      )}
+    </>
+  );
+};
+
+// useGameLogic Hook
+const useGameLogic = () => {
+  const [attempts, setAttempts] = useState<number>(0);
+  const [win, setWin] = useState<boolean>(false);
+  const [theme, setTheme] = useState<string>("");
+  const [kards, setKards] = useState<IKard[]>([]);
+  const [startGame, setStartGame] = useState<boolean>(false);
+
+  const handleGenerateKards = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    // Replace the following block with your actual logic for generating cards
+    // Simulated code:
+    const newKards: IKard[] = Array.from({ length: 10 }, (_, index) => ({
+      id: index + 1,
+      flipped: false,
+      matched: false,
+      defaultImage: "/path-to-default-image.jpg",
+      image: "/path-to-image.jpg",
+    }));
+
+    setKards(newKards);
+    setAttempts(0);
+    setWin(false);
+    setStartGame(true);
+  };
+
+  return {
+    attempts,
+    win,
+    theme,
+    kards,
+    startGame,
+    setTheme,
+    handleGenerateKards,
+    setAttempts,
+    setWin,
+  };
+};
+
+export default Game;
--- sarvgangevani/src/app/quiz/page.tsx
+++ sarvgangevani/src/app/quiz/page.tsx
@@ -57,159 +57,159 @@ const quizData: QuizData[] = [
     ],
     answer: "Salutations to the Ganges",
   },
-  {
-    question:
-      "Which government department is responsible for the implementation of the Namami Gange program?",
-    options: [
-      "Ministry of Health",
-      "Ministry of Environment, Forest and Climate Change",
-      "Ministry of Water Resources, River Development, and Ganga Rejuvenation",
-      "Ministry of Transportation",
-    ],
-    answer:
-      "Ministry of Water Resources, River Development, and Ganga Rejuvenation",
-  },
-  {
-    question: "What is the main cause of pollution in the Ganges River?",
-    options: [
-      "Industrial waste",
-      "Agricultural runoff",
-      "Religious rituals",
-      "All of the above",
-    ],
-    answer: "All of the above",
-  },
-  {
-    question: "Which state in India is the source of the Ganges River?",
-    options: ["Uttar Pradesh", "Himachal Pradesh", "Uttarakhand", "Bihar"],
-    answer: "Uttarakhand",
-  },
-  {
-    question:
-      "What is the role of the National Mission for Clean Ganga (NMCG) in the Namami Gange initiative?",
-    options: [
-      "Promoting tourism along the Ganges",
-      "Implementing projects for the cleaning and conservation of the Ganges",
-      "Regulating fishing activities in the river",
-      "Constructing new bridges across the Ganges",
-    ],
-    answer:
-      "Implementing projects for the cleaning and conservation of the Ganges",
-  },
-  {
-    question:
-      "Which famous wildlife sanctuary is located along the banks of the Ganges River?",
-    options: [
-      "Jim Corbett National Park",
-      "Sundarbans National Park",
-      "Kaziranga National Park",
-      "Ranthambhore National Park",
-    ],
-    answer: "Jim Corbett National Park",
-  },
-  {
-    question:
-      "What is the name of the campaign launched under Namami Gange to promote public awareness and participation?",
-    options: [
-      "Clean Ganga Champions",
-      "Ganga Sankalp Abhiyan",
-      "River Warriors Initiative",
-      "Green Ganges Movement",
-    ],
-    answer: "Ganga Sankalp Abhiyan",
-  },
-  {
-    question:
-      "What are some of the measures taken under the Namami Gange initiative to control industrial pollution in the Ganges?",
-    options: [
-      "Setting up Effluent Treatment Plants (ETPs)",
-      "Promoting eco-friendly industries",
-      "Monitoring industrial discharge",
-      "All of the above",
-    ],
-    answer: "All of the above",
-  },
-  {
-    question: "Which of the following is NOT a tributary of the Ganges River?",
-    options: ["Yamuna", "Brahmaputra", "Gandak", "Godavari"],
-    answer: "Godavari",
-  },
-  {
-    question: "What is the significance of the Ganges River in Hindu rituals?",
-    options: [
-      "It is used for irrigation",
-      "It is considered the abode of a sacred river goddess",
-      "It is a source of drinking water",
-      "It is used for transportation",
-    ],
-    answer: "It is considered the abode of a sacred river goddess",
-  },
-  {
-    question:
-      "Which historical figure is associated with the cleaning and maintenance of the Ganges River?",
-    options: [
-      "Mahatma Gandhi",
-      "Jawaharlal Nehru",
-      "Swami Vivekananda",
-      "Bal Gangadhar Tilak",
-    ],
-    answer: "Mahatma Gandhi",
-  },
-  {
-    question:
-      "What is the name of the action plan launched under the Namami Gange initiative to address the pollution in the Ganges?",
-    options: [
-      "Clean Ganga Project",
-      "Ganga Action Plan (GAP)",
-      "Ganges Conservation Initiative",
-      "Ganges Revitalization Scheme",
-    ],
-    answer: "Ganga Action Plan (GAP)",
-  },
-  {
-    question: "How many states does the Ganges River flow through in India?",
-    options: ["4", "5", "6", "7"],
-    answer:
-      "6 (Uttarakhand, Uttar Pradesh, Bihar, Jharkhand, West Bengal, and a small part of Delhi)",
-  },
-  {
-    question:
-      "Which of the following is a traditional boat race that takes place on the Ganges River?",
-    options: [
-      "Kumbh Mela",
-      "Ganga Dussehra",
-      "Ganga Sagar Mela",
-      "Nag Nathaiya",
-    ],
-    answer: "Nag Nathaiya",
-  },
-  {
-    question:
-      "What is the economic significance of the Ganges River for the local communities along its banks?",
-    options: ["Fishing", "Agriculture", "Tourism", "All of the above"],
-    answer: "All of the above",
-  },
-  {
-    question:
-      "Which of the following is a traditional practice associated with the Ganges River where lamps are floated on the water?",
-    options: [
-      "Ganga Aarti",
-      "Ganges Puja",
-      "Ganges Vandana",
-      "Ganges Pradakshina",
-    ],
-    answer: "Ganga Aarti",
-  },
-  {
-    question:
-      "What is the importance of the Ganges River for agriculture in the region?",
-    options: [
-      "It provides irrigation water",
-      "It controls floods",
-      "It serves as a barrier against pests",
-    ],
-    answer: "It provides irrigation water",
-  },
+  //   {
+  //     question:
+  //       "Which government department is responsible for the implementation of the Namami Gange program?",
+  //     options: [
+  //       "Ministry of Health",
+  //       "Ministry of Environment, Forest and Climate Change",
+  //       "Ministry of Water Resources, River Development, and Ganga Rejuvenation",
+  //       "Ministry of Transportation",
+  //     ],
+  //     answer:
+  //       "Ministry of Water Resources, River Development, and Ganga Rejuvenation",
+  //   },
+  //   {
+  //     question: "What is the main cause of pollution in the Ganges River?",
+  //     options: [
+  //       "Industrial waste",
+  //       "Agricultural runoff",
+  //       "Religious rituals",
+  //       "All of the above",
+  //     ],
+  //     answer: "All of the above",
+  //   },
+  //   {
+  //     question: "Which state in India is the source of the Ganges River?",
+  //     options: ["Uttar Pradesh", "Himachal Pradesh", "Uttarakhand", "Bihar"],
+  //     answer: "Uttarakhand",
+  //   },
+  //   {
+  //     question:
+  //       "What is the role of the National Mission for Clean Ganga (NMCG) in the Namami Gange initiative?",
+  //     options: [
+  //       "Promoting tourism along the Ganges",
+  //       "Implementing projects for the cleaning and conservation of the Ganges",
+  //       "Regulating fishing activities in the river",
+  //       "Constructing new bridges across the Ganges",
+  //     ],
+  //     answer:
+  //       "Implementing projects for the cleaning and conservation of the Ganges",
+  //   },
+  //   {
+  //     question:
+  //       "Which famous wildlife sanctuary is located along the banks of the Ganges River?",
+  //     options: [
+  //       "Jim Corbett National Park",
+  //       "Sundarbans National Park",
+  //       "Kaziranga National Park",
+  //       "Ranthambhore National Park",
+  //     ],
+  //     answer: "Jim Corbett National Park",
+  //   },
+  //   {
+  //     question:
+  //       "What is the name of the campaign launched under Namami Gange to promote public awareness and participation?",
+  //     options: [
+  //       "Clean Ganga Champions",
+  //       "Ganga Sankalp Abhiyan",
+  //       "River Warriors Initiative",
+  //       "Green Ganges Movement",
+  //     ],
+  //     answer: "Ganga Sankalp Abhiyan",
+  //   },
+  //   {
+  //     question:
+  //       "What are some of the measures taken under the Namami Gange initiative to control industrial pollution in the Ganges?",
+  //     options: [
+  //       "Setting up Effluent Treatment Plants (ETPs)",
+  //       "Promoting eco-friendly industries",
+  //       "Monitoring industrial discharge",
+  //       "All of the above",
+  //     ],
+  //     answer: "All of the above",
+  //   },
+  //   {
+  //     question: "Which of the following is NOT a tributary of the Ganges River?",
+  //     options: ["Yamuna", "Brahmaputra", "Gandak", "Godavari"],
+  //     answer: "Godavari",
+  //   },
+  //   {
+  //     question: "What is the significance of the Ganges River in Hindu rituals?",
+  //     options: [
+  //       "It is used for irrigation",
+  //       "It is considered the abode of a sacred river goddess",
+  //       "It is a source of drinking water",
+  //       "It is used for transportation",
+  //     ],
+  //     answer: "It is considered the abode of a sacred river goddess",
+  //   },
+  //   {
+  //     question:
+  //       "Which historical figure is associated with the cleaning and maintenance of the Ganges River?",
+  //     options: [
+  //       "Mahatma Gandhi",
+  //       "Jawaharlal Nehru",
+  //       "Swami Vivekananda",
+  //       "Bal Gangadhar Tilak",
+  //     ],
+  //     answer: "Mahatma Gandhi",
+  //   },
+  //   {
+  //     question:
+  //       "What is the name of the action plan launched under the Namami Gange initiative to address the pollution in the Ganges?",
+  //     options: [
+  //       "Clean Ganga Project",
+  //       "Ganga Action Plan (GAP)",
+  //       "Ganges Conservation Initiative",
+  //       "Ganges Revitalization Scheme",
+  //     ],
+  //     answer: "Ganga Action Plan (GAP)",
+  //   },
+  //   {
+  //     question: "How many states does the Ganges River flow through in India?",
+  //     options: ["4", "5", "6", "7"],
+  //     answer:
+  //       "6 (Uttarakhand, Uttar Pradesh, Bihar, Jharkhand, West Bengal, and a small part of Delhi)",
+  //   },
+  //   {
+  //     question:
+  //       "Which of the following is a traditional boat race that takes place on the Ganges River?",
+  //     options: [
+  //       "Kumbh Mela",
+  //       "Ganga Dussehra",
+  //       "Ganga Sagar Mela",
+  //       "Nag Nathaiya",
+  //     ],
+  //     answer: "Nag Nathaiya",
+  //   },
+  //   {
+  //     question:
+  //       "What is the economic significance of the Ganges River for the local communities along its banks?",
+  //     options: ["Fishing", "Agriculture", "Tourism", "All of the above"],
+  //     answer: "All of the above",
+  //   },
+  //   {
+  //     question:
+  //       "Which of the following is a traditional practice associated with the Ganges River where lamps are floated on the water?",
+  //     options: [
+  //       "Ganga Aarti",
+  //       "Ganges Puja",
+  //       "Ganges Vandana",
+  //       "Ganges Pradakshina",
+  //     ],
+  //     answer: "Ganga Aarti",
+  //   },
+  //   {
+  //     question:
+  //       "What is the importance of the Ganges River for agriculture in the region?",
+  //     options: [
+  //       "It provides irrigation water",
+  //       "It controls floods",
+  //       "It serves as a barrier against pests",
+  //     ],
+  //     answer: "It provides irrigation water",
+  //   },
 ];
 
 // Define the renderOptions function with radio buttons
